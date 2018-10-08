9a8fbeee4ffa7c7b97aa50aebf9a069288159f95
Vinh Doan-The <vinhdt@funix.edu.vn>
2018-10-08 23:30:06 +0700

    Add HeaderComponent and FooterComponent

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent'
 import { DISHES } from '../shared/dishes.js'
+import Header from './HeaderComponent'
+import Footer from './FooterComponent'
 
 class Main extends Component {
     constructor(props) {
@@ -21,15 +22,12 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/"> Vinh Con Fusion</NavbarBrand>
-                    </div>
-                </Navbar>
+                <Header />
                 <Menu dishes={this.state.dishes}
                     onClick={(dishId) => this.onDishSelect(dishId)} />
                 <DishDetail
                     dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Footer />
             </div>
         );
     }
