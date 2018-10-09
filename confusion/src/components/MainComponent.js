4cd170d0c63639c812642616735e76cee01e554f
Vinh Doan-The <vinhdt@funix.edu.vn>
2018-10-09 23:57:18 +0700

    Complete SPA part 1

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent'
 import { DISHES } from '../shared/dishes.js'
+import { COMMENTS } from '../shared/comments'
+import { PROMOTIONS } from '../shared/promotions'
+import { LEADERS } from '../shared/leaders'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
+import Contact from './ContactComponent'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 class Main extends Component {
@@ -13,7 +17,9 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         }
     }
 
@@ -21,7 +27,9 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured == true)[0]}
+                    promotion={this.state.promotions.filter((promotion) => promotion.featured == true)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured == true)[0]}/>
             );
         }
 
@@ -31,6 +39,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
