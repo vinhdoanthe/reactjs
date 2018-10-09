205bb2564cd3999863422e6a57d8a597d65e0319
Vinh Doan-The <vinhdt@funix.edu.vn>
2018-10-10 00:35:33 +0700

    Complete tutorial SPA part 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -29,7 +29,15 @@ class Main extends Component {
             return (
                 <Home dish={this.state.dishes.filter((dish) => dish.featured == true)[0]}
                     promotion={this.state.promotions.filter((promotion) => promotion.featured == true)[0]}
-                    leader={this.state.leaders.filter((leader) => leader.featured == true)[0]}/>
+                    leader={this.state.leaders.filter((leader) => leader.featured == true)[0]} />
+            );
+        }
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                />
             );
         }
 
@@ -39,6 +47,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
