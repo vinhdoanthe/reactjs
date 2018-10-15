1db67361823b04cf906759db80d6efcc544d43f3
Vinh Doan-The <vinhdt@funix.edu.vn>
2018-10-16 01:26:14 +0700

    React Redux Form validation

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -19,11 +19,7 @@ const mapStateToProps = (state) => {
 }
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-
-    }
-
+    
     render() {
 
         const HomePage = () => {
